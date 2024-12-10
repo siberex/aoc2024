6c61a18912e4a5604767ea6977f116e0829f0edd
Stepan Legachev <gitlog@sib.li>
2024-12-10 12:43:31 +0400

    cleanup

--- day10.js
+++ day10.js
@@ -13,7 +13,6 @@ const isOutOfBounds = (x, y) => x < 0 || y < 0 || x > WIDTH - 1 || y > HEIGHT - 
 function getDirections(x, y) {
     const result = [];
     if ( isOutOfBounds(x, y) ) return result;
-    // console.log(`${x}, ${y}`);
     const v = DATA[x][y];
     if ( !isOutOfBounds(x - 1, y) && DATA[x - 1][y] === v + 1 ) result.push([x - 1, y]);
     if ( !isOutOfBounds(x + 1, y) && DATA[x + 1][y] === v + 1 ) result.push([x + 1, y]);
@@ -31,14 +30,12 @@ DATA.forEach((r, i) => r.forEach((h, j) => {
 console.log(`trailheads: ${trailheads.length}`);
 
 
-let trailhead = trailheads[0];
-
-
 function walkTrail(map, trailhead, score) {
     if (score === undefined) score = 0;
 
     let [x, y] = trailhead;
-    if (map[x][y] === 9) {map[x][y] = -1; return 1 }; // end of the trail
+    // end of the trail, mark map and return
+    if (map[x][y] === 9) {map[x][y] = -1; return 1 };
 
     let forks = getDirections(x, y);
     if (forks.length === 0) return 0; // dead end
@@ -51,32 +48,32 @@ function walkTrail(map, trailhead, score) {
         dir => walkTrail(map, dir, score)
     ).reduce((acc, v) => (acc + v), 0);
     
-    // console.log(forks, scores);
     return scores;
 }
 
+const scores = trailheads.map(trailhead => walkTrail(structuredClone(DATA), trailhead));
+console.log(scores.reduce((acc, v) => (acc + v), 0));
 
-// let [x, y] = trailhead;
-// // console.log(getDirections(x, y));
-// let height = 0;
-// let directions = getDirections(x, y);
-// while (height < 9 && directions.length) {
-//     if (directions.length > 1) {
-//         console.log(directions, x, y);
-//     }
-//     trailhead = directions[0];
-//     [x, y] = trailhead;
-//     // console.log(`${x}, ${y}`);
-//     directions = getDirections(x, y);
-//     height++;
-// }
-// console.log(height, directions);
 
-// console.log(walkTrail(structuredClone(DATA), [3, 3]));
-// console.log(walkTrail(structuredClone(DATA), trailhead));
+function walkTrailRate(trailhead, score) {
+    if (score === undefined) score = 0;
 
-let res1 = trailheads.map(
-    th => walkTrail(structuredClone(DATA), th)
-).reduce((acc, v) => (acc + v), 0);
+    let [x, y] = trailhead;
+    if (DATA[x][y] === 9) return 1; // end of the trail
 
-console.log(res1);
+    let forks = getDirections(x, y);
+    if (forks.length === 0) return 0; // dead end
+
+    // only one way forward
+    if (forks.length === 1) return walkTrailRate(forks[0], score);
+
+    // multiple ways forward
+    let scores = forks.map(
+        dir => walkTrailRate(dir, score)
+    ).reduce((acc, v) => (acc + v), 0);
+    
+    return scores;
+}
+
+const ratings = trailheads.map(trailhead => walkTrailRate(trailhead));
+console.log(ratings.reduce((acc, v) => (acc + v), 0));
