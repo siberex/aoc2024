acdf95376db41b8a303dffd2fc1ec6fc72026764
Stepan Legachev <gitlog@sib.li>
2024-12-11 09:50:17 +0400

    naive approach does not work

--- day11.js
+++ day11.js
@@ -7,8 +7,6 @@ const INPUT = await fs.readFile('./input/11.txt', { encoding: 'utf8' });
 const DATA = INPUT.split(' ').map(Number);
 const WIDTH = DATA.length;
 
-// const isOutOfBounds = (x, y) => x < 0 || y < 0 || x > WIDTH - 1 || y > HEIGHT - 1;
-
 // console.log( DATA.map(n => n.toString()).join(' ') ); // debug
 
 const blink = stones => stones.flatMap(n => {
@@ -22,31 +20,49 @@ const blink = stones => stones.flatMap(n => {
     return n * 2024;
 });
 
-
+let len = 0;
 function blink2(stones) {
     for (let i = 0; i < stones.length; i++) {
-        if (stones[i] === 0) {
-            stones[i] = 1;
-            continue;
-        }
-        if (stones[i].toString().length % 2 === 0) {
-            let digits = stones[i].toString().split('');
+        let n = stones[i];
+        if (Array.isArray(n)) {
+            n = blink2(n);
+        } else if (n === 0) {
+            n = 1;
+        } else if (n.toString().length % 2 === 0) {
+            let digits = n.toString().split('');
             let a = parseInt(digits.slice(0, digits.length / 2).join(''));
             let b = parseInt(digits.slice(digits.length / 2, digits.length).join(''));
-            stones[i] = [a, b];
-            continue;
+            n = [a, b];
+        } else {
+            n = n * 2024;
         }
-        stones[i] *= 2024;
+
+        stones[i] = n;
     }
 
-    return stones.flat();
+    return stones;
+}
+
+function getLength(stones) {
+    let len = 0;
+    for (let i = 0; i < stones.length; i++) {
+        let n = stones[i];
+        if (Array.isArray(n)) {
+            len += getLength(n);
+        } else {
+            len += 1;
+        }
+    }
+    return len;
 }
 
 
 // console.log( blink(DATA).map(n => n.toString()).join(' ') ); // debug
 
 // Part 1
+
 let stones = structuredClone(DATA);
+/*
 for (let i = 0; i < 25; i++) {
     stones = blink(stones);
 }
@@ -55,10 +71,36 @@ console.log(stones.length);
 
 console.log('-----------------------------------');
 
+
 stones = structuredClone(DATA);
-for (let i = 0; i < 37; i++) {
+*/
+
+// for (let i = 0; i < 37; i++) {
+//     stones = blink2(stones);
+//     console.log(i);
+// }
+
+for (let i = 0; i < 75; i++) {
     stones = blink2(stones);
     console.log(i);
 }
+console.log(getLength(stones));
 
-console.log(stones.length);
+// console.log(blink2(stones));
+
+
+/*
+On 38th iteration:
+
+<--- Last few GCs --->
+
+[96142:0x7facc2200000]    88597 ms: Mark-Compact 4052.9 (4139.2) -> 4041.0 (4143.0) MB, pooled: 0 MB, 1874.39 / 0.00 ms  (average mu = 0.170, current mu = 0.116) allocation failure; scavenge might not succeed
+[96142:0x7facc2200000]    92135 ms: Mark-Compact 4056.7 (4143.0) -> 4043.8 (4146.0) MB, pooled: 0 MB, 3506.49 / 0.00 ms  (average mu = 0.108, current mu = 0.009) allocation failure; scavenge might not succeed
+
+
+<--- JS stacktrace --->
+
+FATAL ERROR: Reached heap limit Allocation failed - JavaScript heap out of memory
+----- Native stack trace -----
+...
+*/
