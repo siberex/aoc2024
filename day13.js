53233e3d017f9a44571030408e63867814616f2f
Stepan Legachev <gitlog@sib.li>
2024-12-13 10:47:17 +0400

    add day13 part1

--- day13.js
+++ day13.js
@@ -41,40 +41,89 @@ const wins = MACHINES.map(machine => {
 console.log(wins.reduce((acc, v)=>acc+v, 0));
 */
 
-const wins = MACHINES.map(machine => {
-    const {ax, ay, bx, by, px, py} = machine;
+
+const lineCrosses = MACHINES.map(machine => {
+    let {ax, ay, bx, by, px, py} = machine;
+    // ax = BigInt(ax);
+    // ay = BigInt(ay);
+    // bx = BigInt(bx);
+    // by = BigInt(by);
+    // px = BigInt(px);
+    // py = BigInt(py);
+
+    
     const bCnt = (py/ay - px/ax) / (-bx/ax + by/ay);
     const aCnt = (px - bx * bCnt) / ax;
-    const aCnt2 = (py - by * bCnt) / ay;
 
     const aaCnt = (py/by - px/bx) / (-ax/bx + ay/by);
     const bbCnt = (px - ax * aaCnt) / bx;
-    const bbCnt2 = (py - ay * aaCnt) / by;
 
-    if (    bbCnt == Math.round(bbCnt)
-         || aaCnt == Math.round(aaCnt)
-         || bbCnt2 == Math.round(bbCnt2)
-         || aCnt == Math.round(aCnt)
-         || bCnt == Math.round(bCnt)
-         || aCnt2 == Math.round(aCnt2)
-        ) {
-
-        // return [Math.round(aaCnt), Math.round(bbCnt)];
-        return Math.round(bbCnt) * costB + Math.round(aaCnt) * costA;
+    if ( Math.abs(aCnt - Math.round(aCnt)) < 0.0001 && Math.abs(bCnt - Math.round(bCnt)) < 0.0001 ) {
+        return Math.round(bCnt * costB + aCnt * costA);
     } else {
-        // return [aaCnt, bbCnt];
         return -1;
-    };
-}).filter(win => win !== -1);
+    }
 
-// console.log(wins)
+    // if ( bbCnt - Math.round(bbCnt) < 0.01 )
 
+    return [parseInt(aCnt), parseInt(bCnt)];
+
+    // return parseInt(aCnt) * costA, parseInt(bCnt) * costB;
+
+
+    // const aCnt2 = (py - by * bCnt) / ay;
+
+    // const aaCnt = (py/by - px/bx) / (-ax/bx + ay/by);
+    // const bbCnt = (px - ax * aaCnt) / bx;
+    // const bbCnt2 = (py - ay * aaCnt) / by;
+
+    // if (    bbCnt === Math.round(bbCnt)
+    //      || aaCnt === Math.round(aaCnt)
+    //      || bbCnt2 === Math.round(bbCnt2)
+    //      || aCnt === Math.round(aCnt)
+    //      || bCnt === Math.round(bCnt)
+    //      || aCnt2 === Math.round(aCnt2)
+    //     ) {
+
+    //     return [Math.round(aaCnt), Math.round(bbCnt)];
+    //     // return Math.round(bbCnt) * costB + Math.round(aaCnt) * costA;
+    // } else {
+    //     // Problem: { ax: 88, ay: 28, bx: 70, by: 79, px: 8992, py: 5584 }
+
+    //     //  64.00000000000001, 47.999999999999986
+    //     return [machine, aaCnt, bbCnt, bbCnt2, aCnt, aCnt2, bCnt];
+    //     // return -1;
+    // };
+})//.filter(win => win !== -1);
+
+// lineCrosses.forEach(res => {
+//     console.log(res);
+// });
+/*
+let results = MACHINES.map(machine => {
+    const {ax, ay, bx, by, px, py} = machine;
+    const bCnt = (py/ay - px/ax) / (-bx/ax + by/ay);
+    const aCnt = (px - bx * bCnt) / ax;
+    return bCnt * costB + aCnt * costA;
+});
+
+// console.log(results);
+console.log(
+    results.filter(res => {
+        return Math.round(res - 0.0001) === res
+    }).reduce((acc, v)=>acc+v, 0)
+);
+*/
+
+///////////////
+const wins = lineCrosses.filter(win => win !== -1);
 console.log(wins.reduce((acc, v)=>acc+v, 0));
 
 
 // 27092 — not the right answer
 // 34786 - answer is too low
 // 36598 - answer is too low
+// 30448 ?!
 
 
 // N = (py/bx - px/ax) / (-bx/ax + by/ay)
