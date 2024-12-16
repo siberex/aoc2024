497eb432af68b4a8ef78d931b4563116e654e2b9
Stepan Legachev <gitlog@sib.li>
2024-12-17 02:00:07 +0400

    cleanup

--- day11.js
+++ day11.js
@@ -4,6 +4,9 @@ import fs from 'node:fs/promises';
 
 const INPUT = await fs.readFile('./input/11.txt', { encoding: 'utf8' });
 
+// Test1 (11.test) data: p1 = 125681, p2 = 149161030616311
+// Test2 (11.test2): p1 = 55312, p2 = 65601038650482
+
 const DATA = INPUT.split(' ').map(Number);
 
 
@@ -59,6 +62,7 @@ console.log(cnt25);
 
 console.log('-----------------------------------');
 
+// Part 2
 stones = structuredClone(DATA);
 for (let i = 0; i < 25; i++) {
     stones = stones.flatMap(blinkMemo);
@@ -73,68 +77,12 @@ const counts = stones.map((stone, i) => {
         for (let i = 0; i < 25; i++) {
             stones2 = stones2.flatMap(blinkMemo);
         }
-        // MEMO_D25.set(stone, stones2);
+        MEMO_D25.set(stone, stones2);
         MEMO_CNT.set(stone, stones2.length);
     }
 
-    if (i % 100 === 0) console.log(i);
+    if (i % 1000 === 0) console.log(i);
     return stones2.reduce((acc, stone) => acc + expandStoneDepth25(stone), 0);
 });
 
 console.log( counts.reduce((acc, cnt) => acc + cnt, 0) );
-
-
-
-
-/*
-const level1count = DATA.reduce((acc, stone, i) => {
-    if (memo.has(stone)) return acc + memo.get(stone);
-    let stones = [stone];
-    for (let i = 0; i < 25; i++) {
-        stones = blink(stones);
-    }
-    memo.set(stone, stones.length);
-    return acc + stones.length;
-}, 0);
-console.log(level1count);
-console.log('-----------------------------------');
-
-let level2count = DATA.reduce((acc, stone, i) => {
-    if (i % 100 === 0) console.log(i); // debug
-
-    // if (memo.has(stone)) return acc + memo.get(stone);
-    let level2stones = [stone];
-    for (let i = 0; i < 25; i++) {
-        level2stones = blink(level2stones);
-    }
-    if (!memo.has(stone))
-        memo.set(stone, level2stones.length);
-
-    let level3count = level2stones.reduce((cnt, st) => {
-        if (memo.has(st)) return cnt + memo.get(st);
-        let level3stones = [st];
-        for (let i = 0; i < 25; i++) {
-            level3stones = blink(level3stones);
-        }
-        memo.set(st, level3stones.length);
-        return cnt + level3stones.length;
-    }, 0);
-
-    return acc + level3count;
-    // return acc + level2stones.length;
-}, 0);
-
-console.log(level2count);
-*/
-
-
-
-
-// 112979377079  —  answer is too low
-// 432369688950  —  answer is too low
-// 7581573131 ×
-
-// Test data shoule be: 65601038650482
-//            I've got: 3461232181
-//                    : 3433074592 - even more wrong
-//                    : 71602155920
