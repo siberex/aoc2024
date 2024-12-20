f6562f06d4f4ff3cb2f409fac7f987dcff9f5cf7
Stepan Legachev <gitlog@sib.li>
2024-12-21 03:08:28 +0400

    refactor

--- day20.js
+++ day20.js
@@ -121,7 +121,7 @@ shortest_path.forEach((node, index) => {
     }
 });
 
-// console.log(cheats_savings); // debug
+// if (DEBUG) console.log(cheats_savings); // debug
 
 let total = 0;
 for (const saved_picos in cheats_savings) {
@@ -148,16 +148,6 @@ const manhattan = (current, goal) => {
     return d1 + d2;
 }
 
-/*
-const pathMap = shortestPathCoordMap.map((row, y) => row.map((v, x) => ({
-    x,
-    y,
-    v: v === null ? '.' : 'O',
-})));
-
-console.log( printMap(shortestPathCoordMap) );
-*/
-
 // Iterate over shortest path from 0 to end, and from i to end.
 // Check how many picoseconds can be saved by circumventing the path.
 total = 0;
@@ -180,24 +170,24 @@ for (let i = 0; i < shortest_path.length - min_saving; i++) {
 
         if (saving < min_saving) continue;
 
-        total += saving;
+        total += 1;
         megacheats_tested[key] = true;
 
-        // if (DEBUG) {
+        if (DEBUG) {
             if (megacheats_savings[saving]) {
                 megacheats_savings[saving].push(key);
             } else {
                 megacheats_savings[saving] = [key];
             }
-        // }
+        }
     }
 };
 
+// if (DEBUG) console.log(megacheats_savings);
+console.log(total);
 
-// console.log(megacheats_savings);
-
-// if (DEBUG) {
-let total_checksum = 0;
+if (DEBUG) {
+    let total_checksum = 0;
     for (const saved_picos in megacheats_savings) {
         const cheatlist = megacheats_savings[saved_picos];
         // console.log(`There are ${cheatlist.length} cheats that save ${saved_picos} picoseconds.`); // debug:
@@ -205,21 +195,11 @@ let total_checksum = 0;
             total_checksum += cheatlist.length;
         }
     }
-// }
-
-// 662270746 - answer is too high
-// 646090872 - answer is too high
-// 644091669 (-1 for dist) - answer is too high
-console.log(total);
-console.log(total_checksum); // ✓ 990096
+    // console.log(total_checksum); // ✓ 990096
+}
 
 
 // for (const pathId of cheats_savings[2]) {
 //     console.log(pathId);
 // }
 // console.log( printMap(MAP) + '\n' ); // debug
-
-
-    // if (saved_picos >= 50) {
-    //     console.log(`There are ${cheatlist.length} cheats that save ${saved_picos} picoseconds.`);
-    // }
--- day7.js
+++ day7.js
@@ -1,5 +1,7 @@
 // Day 7
 
+import {generatePermutations} from './_utils.js';
+
 import fs from 'node:fs/promises';
 const INPUT = await fs.readFile('./input/7.txt', { encoding: 'utf8' });
 
@@ -9,34 +11,16 @@ const RECORDS = INPUT.split('\n').map(r => {
 });
 
 
-function generatePermutations(n, base) {
-    if (!base) base = 2;
-    const digit = (base - 1).toString();
-  
-    // Max integer of the requested base of the length = n
-    var maxN = parseInt(digit.repeat(n), base);
-  
-    const states = [];
-    // For every int between 0 and max integer of that base
-    for (let i = 0; i <= maxN; i++) {
-        // Convert to binary, pad with 0, and push to the result
-        states.push( i.toString(base).padStart(n, '0').split('') );
-    }
-  
-    return states;
-}
-
-
 // Part 1
 const correct = RECORDS.filter(r => {
     let [total, list] = r;
-    const opsPermutations = generatePermutations(list.length - 1);
+    const opsPermutations = generatePermutations(list.length - 1, 2, true);
 
     for (const ops of opsPermutations) {
         const val = ops.reduce((val, op, i) => {
             // It is safe to expect i+1 from the list: ops will allways be one less than the list
             const next = list[i + 1];
-            return op === '0' ? val + next : val * next;
+            return op === 0 ? val + next : val * next;
         }, list[0]);
         if (val === total) return true;
     }
@@ -51,16 +35,16 @@ console.log(res);
 // Part 2
 const correct2 = RECORDS.filter(r => {
     let [total, list] = r;
-    const opsPermutations = generatePermutations(list.length - 1, 3);
+    const opsPermutations = generatePermutations(list.length - 1, 3, true);
 
     for (const ops of opsPermutations) {
         const val = ops.reduce((val, op, i) => {
             // It is safe to expect i+1 from the list: ops will allways be one less than the list
             const next = list[i + 1];
             switch (op) {
-                case '0': return val + next;
-                case '1': return val * next;
-                case '2': return val * (10 ** next.toString().length) + next;
+                case 0: return val + next;
+                case 1: return val * next;
+                case 2: return val * (10 ** parseInt(Math.log10(next)+1)) + next;
             }
         }, list[0]);
         if (val === total) return true;
