691cb802c27100bb97a4e7d285f8f9d9708d9b46
Stepan Legachev <gitlog@sib.li>
2024-12-21 09:56:33 +0400

    draft day 21 part 1

--- day21.js
+++ day21.js
@@ -26,7 +26,8 @@ const DOOR_CODES = INPUT.split('\n').map(row => row.split(''));
 
 
 
-let code = '029A';
+let code = '029A'.split('');
+let codeNumeic = Number( code.join('').replace('A', '') );
 
 const NUMPAD = [
     ['7',   '8',    '9'],
@@ -42,8 +43,8 @@ NUMPAD.map((row, y) => row.map((key, x) => {
     NUMPAD_MAP.set(key, [x, y]);
 }));
 
-console.log(MAP_NUMPAD);
-console.log(NUMPAD_MAP);
+console.log('MAP_NUMPAD', MAP_NUMPAD); // debug
+console.log('NUMPAD_MAP', NUMPAD_MAP); // debug
 
 const ARROWPAD = [
     [null,  '^',    'A'],
@@ -58,6 +59,48 @@ ARROWPAD.map((row, y) => row.map((key, x) => {
 }));
 
 
-let posN = NUMPAD_MAP.get('A');
+let pos = NUMPAD_MAP.get('A');
+
+code.forEach(targetKey => {
+    const [x0, y0] = pos;
+    const [x, y] = NUMPAD_MAP.get(targetKey);
+    
+    const dx = x - x0;
+    const dy = y - y0;
+
+    let commands = '';
+
+    if (dx === 0) {
+        if (dy > 0) commands += 'v'.repeat(dy);
+        if (dy < 0) commands += '^'.repeat(-dy);
+    } else if (dy === 0) {
+        if (dx > 0) commands += '>'.repeat(dx);
+        if (dx < 0) commands += '<'.repeat(-dx);
+    } else {
+        if (dx > 0 && dy > 0) {
+            commands += '>'.repeat(dx); // important to first move horizontally
+            commands += '^'.repeat(dy);
+        } else if (dx < 0 && dy < 0) {
+            commands += '^'.repeat(-dy); // important to first move vertically
+            commands += '<'.repeat(-dx);
+        } else if (dx < 0 && dy > 0) {
+            commands += '<'.repeat(-dx);
+            commands += 'v'.repeat(dy);
+        } else if (dx > 0 && dy < 0) {
+            commands += '>'.repeat(dx);
+            commands += '^'.repeat(-dy);
+        }
+    }
+
+    commands += 'A';
+    pos = [x, y];
+    console.log(commands);
+
+});
+
+
+
+
+
 let posA = ARROWPAD_MAP.get('A');
 
