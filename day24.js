8af072f6f0057ef3b9cdc029b10c7ece8e5b4b03
Stepan Legachev <gitlog@sib.li>
2024-12-24 11:22:46 +0400

    Day 24 Part 2 begin drafting

--- day24.js
+++ day24.js
@@ -2,8 +2,8 @@
 
 import fs from 'node:fs/promises';
 
-const DEBUG = false;
-const input_filename = DEBUG ? './input/24.test2' : './input/24.txt';
+const DEBUG = true;
+const input_filename = DEBUG ? './input/24.test4' : './input/24.txt';
 const INPUT = await fs.readFile(input_filename, { encoding: 'utf8' });
 
 const [RAW_INITIAL, RAW_GATES] = INPUT.split('\n\n').map(block => block.split('\n'));
@@ -26,8 +26,37 @@ const GATES = RAW_GATES.map(row => {
     return [in1, op, in2, out];
 });
 
-// console.log(WIRES);
+console.log(WIRES);
 // console.log(GATES);
+
+let Xarr = [];
+let Yarr = [];
+let Zarr = [];
+let X = 0n;
+let Y = 0n;
+for (const k in WIRES) {
+    const v = WIRES[k];
+    const group = k.at(0);
+
+    if (group !== 'x' && group !== 'y') continue;
+    
+    const pos = parseInt(k.substring(1));
+    // console.log(k, group, pos);
+
+    switch (group) {
+        case 'x':
+            Xarr[pos] = v;
+            X |= (BigInt(v) << BigInt(pos));
+            break;
+        case 'y':
+            Yarr[pos] = v;
+            Y |= (BigInt(v) << BigInt(pos));
+            break;
+    }
+}
+console.log(X, Xarr);
+console.log(Y, Yarr);
+
 
 function isOutputReady() {
     let res = true;
@@ -59,7 +88,7 @@ while(!isOutputReady()) {
     });
 }
 
-console.log(WIRES);
+// console.log(WIRES);
 
 let DECIMAL = 0n;
 let OUT = [];
@@ -70,9 +99,8 @@ for (const k in WIRES) {
     if (k.at(0) !== 'z') continue;
     const pos = parseInt(k.substring(1));
     
-    DECIMAL = DECIMAL | (BigInt(v) << BigInt(pos));
+    DECIMAL |= (BigInt(v) << BigInt(pos));
     OUT[pos] = v;
 }
 
-console.log(DECIMAL);
-console.log(OUT);
+console.log(DECIMAL, OUT);
