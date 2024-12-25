e7d39e19f9ec74e1888e9824cf5e451a3c9cad96
Stepan Legachev <gitlog@sib.li>
2024-12-25 09:51:48 +0400

    day 25 data parse

--- day25.js
+++ day25.js
@@ -6,8 +6,47 @@ const DEBUG = true;
 const input_filename = DEBUG ? './input/25.test' : './input/25.txt';
 const INPUT = await fs.readFile(input_filename, { encoding: 'utf8' });
 
-const LOCKS_RAW = INPUT.split('\n\n');
+const RAW_BLOCKS = INPUT.split('\n\n');
 
-const LOCKS = LOCKS_RAW.map(block => block.split('\n').map(raw => raw.split('')));
+// console.log(RAW_BLOCKS);
 
-console.log(LOCKS);
+const printArr = arr => arr.map(row => row.join('')).join('\n');
+
+const RAW_LOCKS_AND_KEYS = RAW_BLOCKS.map(block => block.split('\n').map(raw => raw.split('')));
+
+// console.log(RAW_LOCKS_AND_KEYS);
+
+const locks_and_keys = RAW_LOCKS_AND_KEYS.map(lock_or_key => {
+    // Produce rotated matrice
+    const pins_num = 5; // lock_or_key[0].length;
+    const pin_length = 7; // lock_or_key.length;
+    const rotated = Array(pins_num).fill([], 0, pins_num);
+
+    for (let i = 0; i < pins_num; i++) {
+        rotated[i] = Array(pin_length).fill(null, 0, pin_length);
+        
+        for (let j = 0; j < pin_length; j++) {
+        
+            rotated[i][j] = lock_or_key[j][i];
+        }
+    }
+
+    rotated.reverse();
+
+    const isLock = lock_or_key[0].join('') === '#####';
+    rotated.isLock = isLock;
+
+    return rotated;
+});
+
+
+
+const locks = locks_and_keys.filter(lock_or_key => lock_or_key.isLock);
+const keys = locks_and_keys.filter(lock_or_key => !lock_or_key.isLock);
+
+
+console.log('Locks:')
+locks.forEach(lock => console.log( printArr(lock) + '\n' ));
+
+console.log('Keys:')
+keys.forEach(keys => console.log( printArr(keys) + '\n' ));
